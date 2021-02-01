95d24f9589aa96af6ed79355cca1ef30fac834a0
kanaishuuji <72562851+kanaishuuji@users.noreply.github.com>
2021-02-01 22:45:16 +0900

    2章　チェックテスト

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -5,15 +5,16 @@ public class Check {
     private static String firstName="金井";
     private static String lastName="修司";
     public static void main(String[] args) {
-
-         fulname();
+        System.out.println( printName( firstName, lastName));
          Pet k =new Pet (Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
          k.introduce();
+
          RobotPet s =new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
          s.introduce();
+
     }
-    public static void fulname() {
-        System.out.println( "printNameメソッド  →"+firstName + lastName);
+    public static String printName(String firstName,String lastName) {
+       return firstName + lastName;
     }
 }
 
